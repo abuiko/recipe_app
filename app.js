2dc498bf218786347bb20ba3f403111f60090acf
Alena Buiko <abuiko.ma@gmail.com>
2021-02-23 21:43:50 -0500

    convert into async

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-function getRandomMeal() {
-    fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+getRandomMeal();
+
+
+async function getRandomMeal() {
+    const resp = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+
+    const respData = await resp.json();
+    const randomMeal = respData.meals[0];
+    console.log(randomMeal);
+
+    loadRandomMeal();
 }
 
-function getMealById(id) {
-    fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id);
+async function getMealById(id) {
+    const meal = await fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id);
 }
 
-function getMealsBySearch(term) {
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + term);
+async function getMealsBySearch(term) {
+    const meals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + term);
 }
