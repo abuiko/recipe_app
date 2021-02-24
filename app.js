d575eb31b9f3a9b50c2608cf96a4dd74bec805b9
Alena Buiko <abuiko.ma@gmail.com>
2021-02-23 21:54:15 -0500

    change random meal onload of page

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const meals = document.getElementById('meals');
+
+
+
+
 getRandomMeal();
 
 
@@ -6,9 +11,8 @@ async function getRandomMeal() {
 
     const respData = await resp.json();
     const randomMeal = respData.meals[0];
-    console.log(randomMeal);
 
-    loadRandomMeal();
+    addMeal(randomMeal, true);
 }
 
 async function getMealById(id) {
@@ -17,4 +21,26 @@ async function getMealById(id) {
 
 async function getMealsBySearch(term) {
     const meals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + term);
+}
+
+function addMeal(mealData, random = false) {
+    const meal = document.createElement('div');
+    meal.classList.add('meal');
+    meal.innerHTML = `
+                          
+        <div class="meal-header">
+            ${random ? `<span class="random">
+            Random Recipe
+            </span>` : ''}
+            <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
+        </div>
+        <div class="meal-body">
+            <h4>${mealData.strMeal}</h4>
+            <button>
+                <i class="far fa-heart"></i>
+            </button>
+        </div>
+    `;
+
+    meals.appendChild(meal);
 }
